d2c75530f197ee5aa6ed78b58bd1598037fd727a
teguh2116 <43785306+teguh2116@users.noreply.github.com>
2019-01-06 06:36:03 +0700

    Add files via upload

---
+++ Contoh Array 6 karakter yang tidak di inisialisasikan.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    char h[5];
+    for ( int i=0;i<7;i++)
+    {
+        printf("Elemen ke-%i=%c\n",i,h[i]);
+    }
+    getch();
+    return 0;
+}
